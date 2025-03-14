f72ecfd414bc8185aea23745fa37809c06ada048
Nicolas Esteban Lopez Novoa <nicoestebannovoa1@gmail.com>
2025-03-14 12:38:36 +0100

    primer commit

---
+++ Practica_clases/src/practica_clases/Asignaturas.java
@@ -0,0 +1,59 @@
+package practica_clases;
+public class Asignaturas {
+    private int numero_horas;
+    private String profesor;
+    private int codigo;
+    private String nombre;
+    
+    Asignaturas(){
+        this.numero_horas = 0;
+        this.profesor = "";
+        this.codigo = 0;
+        this.nombre = "";
+    }
+    
+    Asignaturas(int num, int cod, String nom){
+        this.numero_horas = num;
+        this.profesor = "";
+        this.codigo = cod;
+        this.nombre = nom;
+    }
+    public String toString(){
+        String str = "";
+        str += this.nombre + " " + this.numero_horas + " " + this.codigo;
+        return str;
+    }
+    public int getNumero_horas() {
+        return numero_horas;
+    }
+
+    public void setNumero_horas(int numero_horas) {
+        this.numero_horas = numero_horas;
+    }
+
+    public String getProfesor() {
+        return profesor;
+    }
+
+    public void setProfesor(String profesor) {
+        this.profesor = profesor;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    
+}
---
+++ Practica_clases/src/practica_clases/Practica_clases.java
@@ -0,0 +1,21 @@
+package practica_clases;
+import java.util.Scanner;
+public class Practica_clases {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Asignaturas a = CrearAsignatura();
+        System.out.println(a.toString());
+    }
+    public static Asignaturas CrearAsignatura(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("por favor ingrese el nombre:");
+        String nombre = sc.nextLine();
+        System.out.println("por favor ingrese el codigo:");
+        int cod = sc.nextInt();
+        System.out.println("por favor ingrese el numero de horas de la asignatura:");
+        int h = sc.nextInt();
+        Asignaturas asig = new Asignaturas(h,cod,nombre);
+        System.out.println(asig.toString());
+        return asig;
+    }
+}
